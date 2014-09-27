650a70bd68fab698d94ed7d664b5c3d4bc984c7b
deds1014 <deds1014@hotmail.com>
2014-09-27 22:39:36 +0800

    Create fib.s

---
+++ lab-1/fib.s
@@ -0,0 +1,40 @@
+	.syntax unified
+	.arch armv7-a
+	.text
+	.align 2
+	.thumb
+	.thumb_func
+
+	.global fibonacci
+	.type fibonacci, function
+
+fibonacci:
+	push {r3, r4, r5, lr}
+
+	subs r4, r0, #0
+	ble .L3
+	
+	cmp r4, #1
+	beq .L4
+
+	add r0, r4, #4294967295 ; r0 = r4 - 1
+	bl fibonacci
+
+	mov r5, r0
+	sub r0, r4, #2 ; r0 = r4 - 2
+	bl fibonacci
+
+	adds r0, r5, r0
+	pop {r3, r4, r5, pc}		@EPILOG
+
+	@ END CODE MODIFICATION
+.L3:
+	mov r0, #0			@ R0 = 0
+	pop {r3, r4, r5, pc}		@ EPILOG
+
+.L4:
+	mov r0, #1			@ R0 = 1
+	pop {r3, r4, r5, pc}		@ EPILOG
+
+	.size fibonacci, .-fibonacci
+	.end
